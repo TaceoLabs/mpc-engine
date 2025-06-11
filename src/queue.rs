2ae97f02d97dd7613d09a2d61c0387d31afadd14
Fabian Gruber <fabian.gruber5@gmail.com>
2025-06-11 16:27:53 +0200

    feat: cleanup

--- src/queue.rs
+++ src/queue.rs
@@ -2,13 +2,18 @@ use intmap::IntMap;
 use parking_lot::{Condvar, Mutex};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
+#[derive(Debug)]
+struct Inner<T> {
+    num: usize,
+    queue: IntMap<usize, T>,
+    next_index: usize,
+}
+
 // TODO we could just put num, queue and next_index in a mutex
 #[derive(Debug)]
 pub struct NetworkQueue<T> {
-    num: AtomicUsize,
-    queue: Mutex<IntMap<usize, T>>,
+    inner: Mutex<Inner<T>>,
     cvar: Condvar,
-    next_index: AtomicUsize,
 }
 
 impl<T> NetworkQueue<T> {
@@ -18,55 +23,57 @@ impl<T> NetworkQueue<T> {
             queue.insert(id, item);
         }
         Self {
-            num: AtomicUsize::new(queue.len()),
-            queue: Mutex::new(queue),
+            inner: Mutex::new(Inner {
+                num: queue.len(),
+                queue,
+                next_index: 0,
+            }),
             cvar: Condvar::new(),
-            next_index: AtomicUsize::default(),
         }
     }
 
     pub fn pop(&self) -> (usize, T) {
-        let mut queue = self.queue.lock();
-        let index =
-            self.next_index.fetch_add(1, Ordering::Relaxed) % self.num.load(Ordering::Relaxed);
+        let mut inner = self.inner.lock();
+        let index = inner.next_index % inner.num;
+        inner.next_index += 1;
 
         // we can get woken up if another item was added back,
         // so we loop and check if it was the one we are wating for
-        while queue.get(index).is_none() {
-            self.cvar.wait(&mut queue);
+        while inner.queue.get(index).is_none() {
+            self.cvar.wait(&mut inner);
         }
 
         // we got woken up, item must be present now
         // only main thread can call pop, so no other thread can be here
-        let item = queue.remove(index).expect("must exist");
+        let item = inner.queue.remove(index).expect("must exist");
         (index, item)
     }
 
     pub fn push(&self, index: usize, item: T) {
-        let mut queue = self.queue.lock();
+        let mut inner = self.inner.lock();
 
         // add item back and notfiy main thread if it was wating on the condvar
-        queue.insert(index, item);
+        inner.queue.insert(index, item);
         self.cvar.notify_one();
     }
 
     pub fn remove(&self) -> Option<T> {
-        let mut queue = self.queue.lock();
+        let mut inner = self.inner.lock();
 
-        if queue.is_empty() {
+        if inner.queue.is_empty() {
             return None;
         }
 
-        let index = queue.len() - 1;
-        self.num.fetch_sub(1, Ordering::Relaxed);
-        queue.remove(index)
+        let index = inner.queue.len() - 1;
+        inner.num -= 1;
+        inner.queue.remove(index)
     }
 
     pub fn insert(&self, item: T) {
-        let mut queue = self.queue.lock();
+        let mut inner = self.inner.lock();
 
-        let index = queue.len();
-        self.num.fetch_add(1, Ordering::Relaxed);
-        queue.insert(index, item);
+        let index = inner.queue.len();
+        inner.num += 1;
+        inner.queue.insert(index, item);
     }
 }
